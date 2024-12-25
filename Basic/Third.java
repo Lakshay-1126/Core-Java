0469176933593678523e77a32b3cd80ec0636a13
Lakshay <work.lakshayk@gmail.com>
2024-12-25 16:59:31 +0530

    Five fav food name

---
+++ Basic/Third.java
@@ -0,0 +1,12 @@
+// Five fav food name
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("DAAL_BAATI");
+        System.out.println("Pizza");
+        System.out.println("Burger");
+        System.out.println("Sandwich");
+        System.out.println("Chaumin");
+  
+    }
+}
