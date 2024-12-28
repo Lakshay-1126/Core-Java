c4c72b1d0c802643aa95df194d49a43f1f659c78
Lakshay <work.lakshayk@gmail.com>
2024-12-28 12:30:50 +0530

    45 + 7

---
+++ Basic/Arithmetic.Constants/AddUser1.java
@@ -0,0 +1,9 @@
+// 45 + 7
+
+public class AddUser1 {
+    public static void main(String[] args) {
+        int a = 45, b = 7, c;
+        c = a + b;
+        System.out.println("the value of c is : " + c);
+    }
+}
