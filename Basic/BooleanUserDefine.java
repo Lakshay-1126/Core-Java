5a26124307ac4835c90933f759a945731438efb3
Lakshay <work.lakshayk@gmail.com>
2024-12-28 21:23:06 +0530

    Boolean user define method

---
+++ Basic/BooleanUserDefine.java
@@ -0,0 +1,11 @@
+// Boolean user define method :
+
+import java.util.Scanner;
+public class BooleanUserDefine {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the value :");
+    boolean a=sc.nextBoolean();
+    System.out.println("entered value is : " + a);
+   } 
+}
