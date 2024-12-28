9237a26d0035d741460099ce0c353f9499414748
Lakshay <work.lakshayk@gmail.com>
2024-12-28 21:47:48 +0530

    Short user define method

---
+++ Basic/ShortUserDefine.java
@@ -0,0 +1,12 @@
+// Short user define method :
+
+import java.util.Scanner;
+
+public class ShortUserDefine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        short a = sc.nextShort();
+        System.out.println("Entered value is: " + a);
+    }    
+}
