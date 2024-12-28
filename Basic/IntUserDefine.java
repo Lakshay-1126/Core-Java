5183f53180ec416ff4f2a49bdb95fae8a6190480
Lakshay <work.lakshayk@gmail.com>
2024-12-28 21:42:57 +0530

    Int user define method

---
+++ Basic/IntUserDefine.java
@@ -0,0 +1,12 @@
+// Int user define method :
+
+import java.util.Scanner;
+
+public class IntUserDefine {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the value :");
+    int a=sc.nextInt();
+    System.out.println("The value of a is " + a);   
+    }
+}
