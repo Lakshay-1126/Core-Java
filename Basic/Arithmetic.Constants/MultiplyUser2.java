0a29af35ca0f51a0bd34a1bd83afc04f600b958f
Lakshay <work.lakshayk@gmail.com>
2024-12-28 12:48:28 +0530

    34 * 5

---
+++ Basic/Arithmetic.Constants/MultiplyUser2.java
@@ -0,0 +1,9 @@
+// 34 * 5
+
+public class MultiplyUser2 {
+    public static void main(String[] args) {
+        int a = 34, b = 5, c;
+        c = a * b;
+        System.out.println("the value of c is : " + c);
+    }
+}
