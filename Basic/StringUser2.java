cc74876a2e677d37cbc72c873acafa17498e95af
Lakshay <work.lakshayk@gmail.com>
2024-12-28 21:54:54 +0530

    User input string for more than a word

---
+++ Basic/StringUser2.java
@@ -0,0 +1,12 @@
+// User input string for more than a word :
+
+import java.util.*;
+
+public class StringUser2 {
+public static void main (String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter a paragraph");
+    String s=sc.nextLine();
+    System.out.println(s);
+}    
+}
