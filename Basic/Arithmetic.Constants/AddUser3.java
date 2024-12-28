249256ecfda41ffdf8b4e17b035cce34fbfb8f11
Lakshay <work.lakshayk@gmail.com>
2024-12-28 13:09:38 +0530

    34+67+23+12+6

--- Basic/Arithmetic.Constants/AddUser3.java
+++ Basic/Arithmetic.Constants/AddUser3.java
@@ -4,6 +4,6 @@ public class AddUser3 {
     public static void main(String[] args) {
         int a = 34, b = 67, c = 23, d = 12, e = 6, f;
         f = a + b + c + d + e;
-        System.out.println("the value of c is : " + f);
+        System.out.println("the value of f is : " + f);
     }
 }
