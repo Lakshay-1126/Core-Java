c3b5a01bd191a39acfc9716381a4be0703bc1141
Lakshay <work.lakshayk@gmail.com>
2024-12-29 12:11:08 +0530

    Even and Odd number

---
+++ Decision Making/EvenOdd.java
@@ -0,0 +1,17 @@
+// By user input check number is even or odd
+
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else if(n%2==1){
+            System.out.println("Odd number");
+        }
+    }
+}
