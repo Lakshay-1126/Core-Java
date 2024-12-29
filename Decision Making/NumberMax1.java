2dbc85c38bde643250f3cce5a99b27ce07c79ec4
Lakshay <work.lakshayk@gmail.com>
2024-12-29 11:17:37 +0530

    One number max a>10

---
+++ Decision Making/NumberMax1.java
@@ -0,0 +1,17 @@
+// One number max a>10
+
+import java.util.Scanner;
+
+public class NumberMax1 {
+   public static void main(String [ ]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter one number");
+    int a=sc.nextInt();
+    if(a>10){
+        System.out.print("a is greater than 10");
+       } 
+    else{
+        System.out.print("a is not greater than 10");
+      }
+   }
+}
