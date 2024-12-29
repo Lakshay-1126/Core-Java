e9d47c9ef0aef7073420a016b20aff6848390972
Lakshay <work.lakshayk@gmail.com>
2024-12-29 11:42:11 +0530

    Divisibilty Check

---
+++ Decision Making/Divisibilty.java
@@ -0,0 +1,23 @@
+/* divisible 5 and 11 
+	a)num dono se divide ho jb process ho 
+	b)num kisi se bhi divide ho jb process ho       */
+
+import java.util.*;
+
+public class Divisibilty {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        if (n%5==0 && n%11==0){
+            System.out.println("number is divisible by 5 and 11");
+        }
+        else if (n%5==0 || n%11==0){
+            System.out.println("number is divisble either by 5 or 11");
+        }
+        else {
+            System.out.println("number is not divisble by any of them");
+        }
+    }
+    
+}
