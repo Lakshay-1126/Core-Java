9561206edfea7a4a1f9813562f27f4f68d2a55ca
Lakshay <work.lakshayk@gmail.com>
2024-12-29 11:56:25 +0530

    Swap without third variable

---
+++ Decision Making/Swap1.java
@@ -0,0 +1,18 @@
+// Swap numbers without third variable
+
+import java.util.*;
+
+public class Swap1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+    System.out.println("enter the value of a");
+    int a=sc.nextInt();
+    System.out.println("enter the value of b");
+    int b=sc.nextInt();
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println("value of a is : "+a+" and value of b is : "+b+" after swapping");
+
+}
+}
