e4f3896d93bfb902a629cebf0434a595efca8a89
Lakshay <work.lakshayk@gmail.com>
2025-01-07 12:18:26 +0530

    java program to print hello world

---
+++ OOPs/MethodBasic1.java
@@ -0,0 +1,15 @@
+/* A :
+	method 1 :hello world 
+ */
+
+class A{
+    void show( ){
+        System.out.println("Hello World");
+}
+}
+public class MethodBasic1 {
+    public static void main(String[] args) {
+        A h=new A();
+        h.show( );
+    }
+}
