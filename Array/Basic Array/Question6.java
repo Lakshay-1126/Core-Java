59c3504dd11d3cfbbdf912d9c75d5ed39fdeab67
Lakshay-1126 <work.lakshayk@gmail.com>
2025-02-10 18:55:07 +0530

    First and last element of array

---
+++ Array/Basic Array/Question6.java
@@ -0,0 +1,11 @@
+/* 12,67,46,435,67,385,335,335,34
+
+first element and last element find   */
+
+public class Question6 {
+    public static void main(String[] args) {
+        int a[]={12,67,46,435,67,385,335,335,34};
+            System.out.println(a[0]+" ");
+            System.out.println(a[8]+" ");
+    }
+}
