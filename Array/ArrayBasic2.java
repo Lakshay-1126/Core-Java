96173f8a3d44469da0ddc2f885789d138e7fe535
Lakshay-1126 <work.lakshayk@gmail.com>
2025-02-10 18:16:35 +0530

    Find length of the array

---
+++ Array/ArrayBasic2.java
@@ -0,0 +1,9 @@
+public class ArrayBasic2 {
+    public static void main(String[] args){
+        int[] a={67,89,46,23};
+        //find length of the array 
+       // System.out.println(a.length);
+        int n=a.length;
+        System.out.println(n);
+    }
+}
